8d3590fffefb7a0fb2a2c81b67c410e490a913a0
pnadalini <pietro.nadalini@hotmail.com>
2018-09-01 23:58:39 -0600

    Added http post on Azure website

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -1,222 +1,177 @@
-/*====================================REGION LIBRARIES====================================*/
-#include <Wire.h> //I2C needed for sensors
-#include "SparkFunMPL3115A2.h" //Pressure sensor - Search "SparkFun MPL3115" and install from Library Manager
-#include "SparkFunHTU21D.h" //Humidity sensor - Search "SparkFun HTU21D" and install from Library Manager
-#include <SPI.h>
-#include <WiFi.h>
+/*====================================REGION LIBRARIES===================================*/
+#include <SPI.h> 
+#include <Wire.h>              // I2C needed for sensors
+#include "SparkFunMPL3115A2.h" // Pressure sensor - Search "SparkFun MPL3115" and install from Library Manager
+#include "SparkFunHTU21D.h"    // Humidity sensor - Search "SparkFun HTU21D" and install from Library Manager
+#include <WiFi.h> 
+#include <ArduinoJson.h>       // Json object - Search "ArduinoJson" and install the newest version that's not in beta
 
-/*====================================REGION VARIABLES====================================*/
-MPL3115A2 myPressure; //Create an instance of the pressure sensor
-HTU21D myHumidity; //Create an instance of the humidity sensor
+/*====================================REGION VARIABLES===================================*/
+MPL3115A2 pressureSensor;  // Create an instance of the pressure sensor
+HTU21D humiditySensor;     // Create an instance of the humidity sensor
+WiFiServer server(80);
+WiFiClient client;
 
-//Hardware pin definitions
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-const byte STAT_BLUE = 7;
-const byte STAT_GREEN = 8;
-
+/*==================================== HARDWARE PINS ====================================*/
 const byte REFERENCE_3V3 = A3;
 const byte LIGHT = A1;
 const byte BATT = A2;
 
-//Global Variables
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-long lastSecond; //The millis counter to see when a second rolls by
-
-char ssid[] = "HerreraPoggioAbajo";     //  your network SSID (name) 
-char pass[] = "0ab7da62a6";    // your network password
+/*====================================GLOBAL VARIABLES===================================*/
+char ssid[] = "<ssid>"; // Your network SSID (name) 
+char pass[] = "<password>";         // Your network password
 int status = WL_IDLE_STATUS;
 
-WiFiServer server(80);
+/*====================================ARDUINO METHODS===================================*/
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  Serial.println("Weather Shield Example");
-
-  pinMode(STAT_BLUE, OUTPUT); //Status LED Blue
-  pinMode(STAT_GREEN, OUTPUT); //Status LED Green
+  Serial.println("Initializing components");
 
   pinMode(REFERENCE_3V3, INPUT);
   pinMode(LIGHT, INPUT);
 
-  //Configure the pressure sensor
-  myPressure.begin(); // Get sensor online
-  myPressure.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
-  myPressure.setOversampleRate(7); // Set Oversample to the recommended 128
-  myPressure.enableEventFlags(); // Enable all three pressure and temp event flags
-
-  //Configure the humidity sensor
-  myHumidity.begin();
-
-  lastSecond = millis();
+  configureSensors();
 
   Serial.println("Weather Shield online!");
 
   // start serial port for debugging purposes
-  Serial.begin(9600);
-  
-  // attempt to connect to Wifi network:
-  while ( status != WL_CONNECTED) { 
+  //Serial.begin(9600);
+
+  // Attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
-    // wait 10 seconds for connection:
+    // Wait 10 seconds for connection:
     delay(10000);
-  } 
+  }
   server.begin();
-  // you're connected now, so print out the status:
+  // You're connected now, so print out the status:
   printWifiStatus();
 }
 
 
 void loop() {
-            
-  // listen for incoming clients
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connnection: close");
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<meta http-equiv=\"refresh\" content=\"5\">");
-      
+  // Post the Weather Shield readings to the server
+  post();
+  waitForResponse();
+  readResponse();
+  // Give the web browser time to receive the data and wait time so the next weather reading begins
+  delay(10000);
+  // Close the connection:
+  client.stop();
+  Serial.println("client disonnected");
+  Serial.println("------------------------------------------------");
+}
 
-      //Check Humidity Sensor
-      float humidity = myHumidity.readHumidity();
-          
-      if (humidity == 998) //Humidty sensor failed to respond
-      {
-        Serial.println("I2C communication to sensors is not working. Check solder connections.");
-          
-        //Try re-initializing the I2C comm and the sensors
-        myPressure.begin(); 
-        myPressure.setModeBarometer();
-        myPressure.setOversampleRate(7);
-        myPressure.enableEventFlags();
-        myHumidity.begin();
-      }
-      else
-      {
-        client.print("Humidity = ");
-        client.print(humidity);
-        client.print("%,");
-        float temp_h = myHumidity.readTemperature();
-        client.print(" temp_h = ");
-        client.print(temp_h, 2);
-        client.print("C,");
-          
-        //Check Pressure Sensor
-        float pressure = myPressure.readPressure();
-        client.print(" Pressure = ");
-        client.print(pressure);
-        client.print("Pa,");
-          
-        //Check tempf from pressure sensor
-        float tempf = myPressure.readTempF();
-        client.print(" temp_p = ");
-        client.print(tempf, 2);
-        client.print("F,");
-          
-        //Check light sensor
-        float light_lvl = get_light_level();
-        client.print(" light_lvl = ");
-        client.print(light_lvl);
-        client.print("V,");
-          
-        //Check batt level
-        float batt_lvl = get_battery_level();
-        client.print(" VinPin = ");
-        client.print(batt_lvl);
-        client.print("V");
-          
-        client.println();
-      }
-            client.println("<br />");
-            client.println("</html>");
-            break;
-        }
-        
-    if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } 
-        else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-  
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Serial.println("client disonnected");
+/*====================================CREATED METHODS===================================*/
+
+void configureSensors(){
+  // Configure the pressure sensor
+  pressureSensor.begin(); // Get sensor online
+  pressureSensor.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
+  pressureSensor.setOversampleRate(7); // Set Oversample to the recommended 128
+  pressureSensor.enableEventFlags(); // Enable all three pressure and temp event flags
+
+  // Configure the humidity sensor
+  humiditySensor.begin();
+}
+
+void post() {
+  Serial.println("Connecting to Azure...");
+
+  if (client.connect("<function_name>.azurewebsites.net", 80)) {
+    Serial.println("Conected!, preparing post...");
+    client.println("POST /api/<trigger_name> HTTP/1.1");
+    client.println("Host: <function_name>.azurewebsites.net");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    getWeatherReadings();
+  } else {
+    Serial.println("connection failed");
   }
 }
 
+void getWeatherReadings() {
+  // Check Humidity Sensor
+  float humidity = humiditySensor.readHumidity();
 
-//Returns the voltage of the light sensor based on the 3.3V rail
-//This allows us to ignore what VCC might be (an Arduino plugged into USB has VCC of 4.5 to 5.2V)
-float get_light_level()
-{
+  if (humidity == 998) {
+    // Humidty sensor failed to respond
+    Serial.println("I2C communication to sensors is not working. Check solder connections.");
+
+    //Try re-initializing the I2C comm and the sensors
+    configureSensors();
+  } else {
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject &postMessage = jsonBuffer.createObject();
+
+    // Check all reading (Temperature, Pressure, Light)
+    float tempC = humiditySensor.readTemperature();
+    float pressure = pressureSensor.readPressure();
+    float tempf = pressureSensor.readTempF();
+    float lightLvl = getLightLevel();
+
+    // Put all readings on the Json Object
+    postMessage["Humidity"] = humidity;
+    postMessage["TempC"] = tempC;
+    postMessage["Pressure"] = pressure;
+    postMessage["TempF"] = tempf;
+    postMessage["LightLvl"] = lightLvl;
+
+    // Write all the readings on the client
+    client.println(postMessage.measureLength());
+    client.println();
+    Serial.println("Json sent to the server:");
+    postMessage.printTo(Serial);
+    Serial.println();
+    postMessage.printTo(client);
+  }
+}
+
+void waitForResponse() {
+  Serial.println("Waiting for response from server..");
+  while (!client.available()) {
+      if (!client.connected()) {
+          return;
+      }
+  }
+}
+
+void readResponse() {
+  Serial.println("*****************RESPONSE******************");
+  while (client.available()) {
+      char c = client.read();
+      Serial.print(c);
+  }
+}
+
+// Returns the voltage of the light sensor based on the 3.3V rail
+// This allows us to ignore what VCC might be (an Arduino plugged into USB has VCC of 4.5 to 5.2V)
+float getLightLevel() {
   float operatingVoltage = analogRead(REFERENCE_3V3);
-
   float lightSensor = analogRead(LIGHT);
-
-  operatingVoltage = 3.3 / operatingVoltage; //The reference voltage is 3.3V
-
+  
+  operatingVoltage = 3.3 / operatingVoltage; // The reference voltage is 3.3V
   lightSensor = operatingVoltage * lightSensor;
 
   return (lightSensor);
 }
 
-//Returns the voltage of the raw pin based on the 3.3V rail
-//This allows us to ignore what VCC might be (an Arduino plugged into USB has VCC of 4.5 to 5.2V)
-//Battery level is connected to the RAW pin on Arduino and is fed through two 5% resistors:
-//3.9K on the high side (R1), and 1K on the low side (R2)
-float get_battery_level()
-{
-  float operatingVoltage = analogRead(REFERENCE_3V3);
-
-  float rawVoltage = analogRead(BATT);
-
-  operatingVoltage = 3.30 / operatingVoltage; //The reference voltage is 3.3V
-
-  rawVoltage = operatingVoltage * rawVoltage; //Convert the 0 to 1023 int to actual voltage on BATT pin
-
-  rawVoltage *= 4.90; //(3.9k+1k)/1k - multiple BATT voltage by the voltage divider to get actual system voltage
-
-  return (rawVoltage);
-}
-
 void printWifiStatus() {
-  // print the SSID of the network you're attached to:
+  // Print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your WiFi shield's IP address:
+  // Print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength:
+  // Print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
+  Serial.print("Signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
 }
